4e59127c6779c4ca6fb07a1298ad76cd496a0768
Jaime Montes de Oca <jaime0422@live.com>
2022-09-06 23:03:21 -0700

    Completed Assignment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,20 +1,27 @@
 class ArticlesController < ApplicationController
-  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
-  def index
-    articles = Article.all.includes(:user).order(created_at: :desc)
-    render json: articles, each_serializer: ArticleListSerializer
-  end
+    def index
+        articles = Article.all.includes(:user).order(created_at: :desc)
+        render json: articles, each_serializer: ArticleListSerializer
+    end
 
-  def show
-    article = Article.find(params[:id])
-    render json: article
-  end
+    def show
+        sessions[:page_views] ||= 0
+        session[:page_views] += 1
 
-  private
+        if session[:page_views] <= 3
+            article = Article.find(params[:id])
+            render json: article
+        else
+            render json: {error: "Maximum pageview limit reached"}, status: :unauthorized
+        end
+    end
 
-  def record_not_found
-    render json: { error: "Article not found" }, status: :not_found
-  end
+    private
+
+    def record_not_found
+        render json: { error: "Article not found" }, status: :not_found
+    end
 
 end
